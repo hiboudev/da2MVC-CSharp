71d9295d8a6f80eaf7a548c9481ec27a173c7e9d
Damien Laurent <hiboudev@gmail.com>
2017-12-12 17:15:55 +0100

    Start of a framework project upon the core MVC.

---
+++ da2Framework/model/CollectionModel.cs
@@ -0,0 +1,32 @@
+﻿using da2MVC.events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da2Framework.model
+{
+    public class CollectionModel<ModelType> : EventDispatcher where ModelType : IModel
+    {
+        private List<ModelType> Collection { get; } = new List<ModelType>();
+
+        public void Add(ModelType model)
+        {
+            Collection.Add(model);
+        }
+
+        public void AddRange(List<ModelType> models)
+        {
+            Collection.AddRange(models);
+        }
+
+        public ModelType Get(int id)
+        {
+            foreach (ModelType model in Collection)
+                if (model.Id == id)
+                    return model;
+            return default(ModelType);
+        }
+    }
+}
---
+++ da2Framework/model/IModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da2Framework.model
+{
+    public interface IModel
+    {
+        int Id { get; }
+    }
+}
