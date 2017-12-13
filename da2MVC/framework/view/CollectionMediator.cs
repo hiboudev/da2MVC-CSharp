1eb43f15d0eef5b90376ec8e83723e6f206e5bb9
Damien Laurent <hiboudev@gmail.com>
2017-12-13 15:08:16 +0100

    CollectionMediator add collection items to view at initialization.

--- da2MVC/framework/view/CollectionMediator.cs
+++ da2MVC/framework/view/CollectionMediator.cs
@@ -1,4 +1,5 @@
 ﻿using da2mvc.core.events;
+using da2mvc.core.injection;
 using da2mvc.core.view;
 using da2mvc.framework.model;
 using da2mvc.framework.model.events;
@@ -11,13 +12,22 @@ using System.Threading.Tasks;
 
 namespace da2mvc.framework.view
 {
-    public class CollectionMediator<CollectionType, ModelType> : BaseMediator<ICollectionView<ModelType>> where CollectionType : CollectionModel<ModelType> where ModelType :IModel
+    public class CollectionMediator<CollectionType, ModelType, ViewType> : BaseMediator<ViewType> where CollectionType : CollectionModel<ModelType> where ModelType :IModel where ViewType : ICollectionView<ModelType>
     {
         public CollectionMediator()
         {
             RegisterEventListener<CollectionEventArgs<ModelType>>(typeof(CollectionType), CollectionModel<ModelType>.EVENT_ITEMS_ADDED, OnItemsAdded);
             RegisterEventListener<CollectionEventArgs<ModelType>>(typeof(CollectionType), CollectionModel<ModelType>.EVENT_ITEMS_REMOVED, OnItemsRemoved);
             RegisterEventListener<CollectionEventArgs<ModelType>>(typeof(CollectionType), CollectionModel<ModelType>.EVENT_CLEARED, OnItemsCleared);
+        }
+
+        protected override void ViewInitialized()
+        {
+            base.ViewInitialized();
+
+            // To avoid forcing sub-classes to declare a ctor parameter.
+            var collection = Injector.GetInstance<CollectionType>();
+            View.Add(collection.Items.ToArray());
         }
 
         private void OnItemsAdded(CollectionEventArgs<ModelType> args)
